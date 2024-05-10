366dae00ca34c1309b64a0ddde3bdd2a10d4c4f0
Tejas Kumar S <tej0897@gmail.com>
2024-05-10 21:06:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,19 @@ function getAverage(scores) {
   return sum / scores.length;
 }
 function getGrade(score) {
-
+    if(score === 100){
+      return "A++";
+    } else if(score >= 90 && score <= 99){
+      return "A";
+    } else if (score >= 80 && score <= 89){
+      return "B";
+    } else if (score >= 70 && score <= 79){
+      return "C";
+    } else if (score >= 60 && score <= 69){
+      return "D";
+    } else {
+      return "F";
+    }
 }
 
 console.log(getGrade(96));
